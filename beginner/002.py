c8ae7753c164fcce435461cc7ec50af5a2aa4183
hspassos <passos.487@gmail.com>
2023-08-07 20:29:53 -0300

    exercicios 1 e 2

---
+++ beginner/002.py
@@ -0,0 +1,6 @@
+print("Welcome to the tip calculator.")
+bill = float(input("What was the total bill? "))
+tip = int(input("What percentage would you like to give? 10, 12 or 15? "))
+people = int(input("How many people to split the bill? "))
+result = "{:.2f}".format((bill * (1+tip/100)) / people)
+print(f"Each person should pay: ${result}")
